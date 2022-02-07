76924e40713bdc1e16509818cb8ee355b2430a6e
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-02-08 01:01:27 +0900

    2022-02-07 수업

--- JavaStudy_3248/src/a16_Lombok/Product.java
+++ JavaStudy_3248/src/a16_Lombok/Product.java
@@ -1,5 +1,13 @@
 package a16_Lombok;
 
+@Builder
+@NoArgaConstructor
+@AllArgsConstructor
+@Data
 public class Product {
+	private int product_code;
+	private String product_name;
+	private String product_category;
+	private String product_date;
 
 }
