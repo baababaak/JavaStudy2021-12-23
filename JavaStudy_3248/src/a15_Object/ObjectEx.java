bf63613e32843489fdcb4f31b2752a234268f55b
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-02-08 00:57:46 +0900

    2022-02-07 수업

---
+++ JavaStudy_3248/src/a15_Object/ObjectEx.java
@@ -0,0 +1,26 @@
+package a15_Object;
+
+/**
+ * 
+ * Object Class: 최상위 클래스
+ * 모든 클래스는 Object 클래스를 상속받고 있다.
+ * 즉, 모든 클래스는 Object 클래스로 업캐스팅이 가능하다.
+ * 그리고 모든 클래스는 Object 클래스의 메소드를 오버라이딩 할 수 있다.
+ *
+ */
+public class ObjectEx {
+
+	public static void main(String[] args) {
+		Object obj = new Object();
+		System.out.println(obj);
+		
+		String str = new String();
+		
+		User user1 = new User("junil", "1234");
+		User user2 = new User("junil", "1234");
+		
+		System.out.println(user1.equals(user2));
+
+	}
+
+}
---
+++ JavaStudy_3248/src/a15_Object/User.java
@@ -0,0 +1,17 @@
+package a15_Object;
+
+/**
+ * 
+ * lombok 어노테이션
+ * 
+ * 
+ *
+ */
+
+
+public class User {
+	private String username;
+	private String pwd;
+
+	
+}
